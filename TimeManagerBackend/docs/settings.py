1cd43ca9cb8384f9bea39a74e74ee0623817c93a
ThaRising <kochbe.ber@gmail.com>
2020-12-06 03:44:51 +0100

    Implemented write_only attribute into OpenAPI schema generation

--- TimeManagerBackend/docs/settings.py
+++ TimeManagerBackend/docs/settings.py
@@ -5,7 +5,9 @@ SWAGGER_SETTINGS = {
             "name": "Authorization",
             "in": "header"
         }
-    }
+    },
+    "DEFAULT_AUTO_SCHEMA_CLASS":
+        "TimeManagerBackend.lib.openapi_schema_gen.ReadWriteAutoSchema",
 }
 
 __all__ = ["SWAGGER_SETTINGS"]
