c753fab7efaf80115ba4d3d1d598f1495dc9408f
ThaRising <kochbe.ber@gmail.com>
2020-12-06 18:06:18 +0100

    Regression fix - remove admin

--- TimeManagerBackend/urls.py
+++ TimeManagerBackend/urls.py
@@ -8,14 +8,13 @@ favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
     # Included URL paths
-    path('admin/', admin.site.urls),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$',
             schema_view.without_ui(cache_timeout=0),
             name='schema-json'),
     re_path(r'^redoc/$',
             schema_view.with_ui('redoc', cache_timeout=0),
             name='schema-redoc'),
-    # Note that eh browsable API auth is absent,
+    # Note that the browsable API auth is absent,
     # because it uses SessionAuth, while we use JWT
     re_path(r'^favicon\.ico$', favicon_view),
 
