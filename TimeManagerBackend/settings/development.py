404267795eade1b16b9b59305f6c358bd1d1b666
ThaRising <kochbe.ber@gmail.com>
2020-12-06 22:29:21 +0100

    Reverted change to settings that caused crash in deployment

--- TimeManagerBackend/settings/development.py
+++ TimeManagerBackend/settings/development.py
@@ -9,6 +9,13 @@ from django.conf.global_settings import (
 
 DEBUG = True
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 MIDDLEWARE = [
     # Activate CORS
     'corsheaders.middleware.CorsMiddleware',
--- TimeManagerBackend/settings/production.py
+++ TimeManagerBackend/settings/production.py
@@ -60,12 +60,22 @@ MIDDLEWARE = [
 
 
 """ ORM Config """
+CLOUD_SQL_CONN_NAME = f"{terraform.vars.project_name}:" \
+                      f"{terraform.vars.project_region}:{terraform.vars.db_service_name}"
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': f"/cloudsql/{CLOUD_SQL_CONN_NAME}",
+        'PORT': "5432",
+        'NAME': terraform.vars.db_name,
+        'USER': terraform.vars.db_username,
+        'PASSWORD': terraform.vars.db_password,
     }
 }
+
+if os.getenv("MIGRATION_MODE"):
+    DATABASES["default"]["HOST"] = "localhost"
 
 
 
@@ -191,5 +201,31 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 """ Load in the Deployment Settings """
-if os.getenv("K_SERVICE"):  # K_SERVICE is an env variable of CloudRun containers
-    from .remote import *
+# if we are running on CloudRun, configure stackdriver logging
+if os.getenv("K_SERVICE"):
+    from google.cloud import logging
+
+    client = logging.Client().from_service_account_json(GS_CREDENTIALS_FILE)
+    # Connects the logger to the root logging handler; by default
+    # this captures all logs at INFO level and higher
+    client.setup_logging()
+    LOGGING = {
+        'version': 1,
+        'handlers': {
+            'stackdriver': {
+                'class': 'google.cloud.logging.handlers.CloudLoggingHandler',
+                'client': client
+            }
+        },
+        'loggers': {
+            'cloud': {
+                'handlers': ['stackdriver'],
+                'level': 'INFO',
+                'name': "cloud"
+            },
+            'django.request': {
+                'handlers': ['stackdriver'],
+                'level': 'WARN',
+            },
+        }
+    }
