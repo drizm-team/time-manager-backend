67c793a9e9893a9c03bdd6572027e69d293cdb69
ThaRising <kochbe.ber@gmail.com>
2020-12-21 23:14:55 +0100

    Added additional testing fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,16 @@
+import base64
+import io
+import random
 from typing import Dict, Tuple, Optional
 
+from PIL import Image
 from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
+
+from TimeManagerBackend.apps.images.models.serializers import UserProfilePictureSerializer
 
 # Default set of credentials for the test user
 TEST_USER_EMAIL = "default_tester@tester.de"
@@ -13,11 +19,19 @@ TEST_USER_PASSWORD = "shittySecurity420!"
 
 def create_test_user(email: Optional[str] = None,
                      password: Optional[str] = None):
+    serializer = UserProfilePictureSerializer(data={
+        "background": random_hex_color()
+    })
+    serializer.is_valid(raise_exception=True)
+    profile_picture = serializer.save()
+
     User = get_user_model()
     user = User.objects.create_user(
         email=email or TEST_USER_EMAIL,
-        password=password or TEST_USER_PASSWORD
+        password=password or TEST_USER_PASSWORD,
+        profile_picture=profile_picture
     )
+    profile_picture.save()
     return user
 
 
@@ -50,7 +64,30 @@ def self_to_id(body: dict):
         return identifier
 
 
+def generate_test_image(ext: Optional[str] = "jpeg") -> io.BytesIO:
+    file = io.BytesIO()
+    image = Image.new(
+        "RGB",
+        size=(192, 192),
+        color=tuple(  # generate a random color
+            random.randint(100, 200) for _ in range(3)
+        )
+    )
+    image.save(file, ext)
+
+    return file
+
+
+def generate_image_b64(image_file: io.BytesIO) -> bytes:
+    return base64.b64encode(image_file.getvalue())
+
+
+def random_hex_color() -> str:
+    return "#%06x" % random.randint(0, 0xFFFFFF)
+
+
 __all__ = [
     "TEST_USER_EMAIL", "TEST_USER_PASSWORD",
-    "create_test_user", "obtain_tokens", "self_to_id"
+    "create_test_user", "obtain_tokens", "self_to_id",
+    "generate_test_image", "generate_image_b64", "random_hex_color"
 ]
