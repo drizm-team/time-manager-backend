5b0d85b2afa2f5d3eb7b07d8e3f34e41bdd75a35
ThaRising <kochbe.ber@gmail.com>
2021-02-26 15:23:08 +0100

    Fixed issues with local docker deployments

--- tests/pre_test.sh
+++ tests/pre_test.sh
@@ -5,18 +5,18 @@ compose_namespace="timemanagertests"
 
 # Remove services from previous tests
 (
-cd ../docker || exit 127
+cd docker || exit 127
 docker-compose -p "$compose_namespace" \
   down --volumes
 )
 
 # Create the test bucket for the GCS-server
-source ../scripts/common.sh
-mkdir -p "../docker/test-gcs-data/$(django_settings GCS_BUCKET_NAME)"
+source scripts/common.sh
+mkdir -p "docker/test-gcs-data/$(django_settings GS_BUCKET_NAME)"
 
 # Start all services required for testing
 (
-cd ../docker || exit 127
+cd docker || exit 127
 docker-compose -p "$compose_namespace" \
   -f docker-compose.yml \
   -f docker-compose.test.yml \
