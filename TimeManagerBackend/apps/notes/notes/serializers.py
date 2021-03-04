6f1ee26192460e462550422a364e90a8a0dc66dc
ThaRising <kochbe.ber@gmail.com>
2021-03-04 20:21:53 +0100

    Finish implementation and testing of notes

--- TimeManagerBackend/apps/notes/notes/serializers.py
+++ TimeManagerBackend/apps/notes/notes/serializers.py
@@ -1,48 +1,116 @@
+from django.contrib.auth import get_user_model
 from django.utils.timezone import now
 from rest_framework import serializers
 
+from TimeManagerBackend.apps.users.models.serializers import UserSerializer
 from TimeManagerBackend.lib.commons.constrained import VersionConstrainedUUIDField
-from TimeManagerBackend.lib.commons.href import SelfHrefField
 from TimeManagerBackend.lib.commons.defaults import CurrentUserPkDefault
+from TimeManagerBackend.lib.commons.firestore import get_firestore, DocumentWrapper
+from TimeManagerBackend.lib.commons.href import SelfHrefField
+from ..boards.models import NotesBoard
+from ..groups.models import NotesGroup
 
 
-class BoardNotesSerializer(serializers.Serializer):  # noqa abstract
+class NotesSerializerMixin:
+    def to_representation(self, instance):
+        self_ = self.fields.get(  # noqa mixin
+            "self"
+        ).to_representation(instance)
+        content = instance.content
+        edited_by = UserSerializer(
+            get_user_model().objects.get(id=instance.edited_by),
+            context=self.context  # noqa mixin
+        ).data
+        last_edited = serializers.DateTimeField().to_representation(
+            instance.last_edited
+        )
+        return {
+            "self": self_,
+            "content": content,
+            "edited_by": edited_by,
+            "last_edited": last_edited
+        }
+
+    # noinspection PyMethodMayBeStatic
+    def update(self, instance, validated_data):
+        instance.reference.set({
+            "last_edited": validated_data.pop("last_edited"),
+            "edited_by": validated_data.pop("edited_by"),
+            "content": validated_data.pop("content")
+        }, merge=True)
+
+        return DocumentWrapper(instance.reference.get())
+
+    def create(self, validated_data):
+        db = get_firestore()
+        pk = str(validated_data.pop("id"))
+        parent_pk = validated_data.pop("parent").pk
+
+        col_ref = db.collection(
+            self.Meta.collection_name, str(parent_pk), "notes"  # noqa mixin
+        )
+        doc_ref = col_ref.document(pk)
+        doc_ref.set(validated_data)
+        return DocumentWrapper(doc_ref.get())
+
+
+class BoardNotesSerializer(NotesSerializerMixin, serializers.Serializer):
     self = SelfHrefField(
         lookup_field="id",
         lookup_url_kwarg="pk",
-        lookup_chain={"board": "parent.id"},
+        lookup_chain={"boards_pk": "reference.parent.parent.id"},
         read_only=True
     )
-    id = VersionConstrainedUUIDField(uuid_version=4)
-
-    last_edited = serializers.HiddenField(default=now, write_only=True)
-    edited_by = serializers.HiddenField(
-        default=CurrentUserPkDefault(), write_only=True
+    id = VersionConstrainedUUIDField(
+        uuid_version=4, write_only=True, required=True
     )
+    parent = serializers.PrimaryKeyRelatedField(
+        queryset=NotesBoard.objects.all()
+    )
+
+    created = serializers.HiddenField(default=now)
+    last_edited = serializers.HiddenField(default=now)
+    edited_by = serializers.HiddenField(default=CurrentUserPkDefault())
     content = serializers.CharField(required=True, allow_blank=True)
 
     class Meta:
-        self_view = "notes:board-note-detail"
-        collection_name = ("notes__notes_board",)
-
-    def create(self, validated_data):
-        pass
-
-    def update(self, instance, validated_data):
-        pass
+        self_view = "notes:boards-notes"
+        collection_name = "notes__boards"
 
 
-class GroupNotesSerializer(BoardNotesSerializer):  # noqa abstract
+def get_boards_pk(obj):
+    parent_id = obj.reference.parent.parent.id
+    group = NotesGroup.objects.get(id=parent_id)
+    return group.parent_id
+
+
+class GroupNotesSerializer(NotesSerializerMixin, serializers.Serializer):
     self = SelfHrefField(
         lookup_field="id",
         lookup_url_kwarg="pk",
-        lookup_chain={"group": "parent.id", "board": "parent.parent.id"},
+        lookup_chain={
+            "boards_pk": get_boards_pk,
+            "groups_pk": "reference.parent.parent.id"
+        },
         read_only=True
     )
+    id = VersionConstrainedUUIDField(
+        uuid_version=4, write_only=True, required=True
+    )
+    parent = serializers.PrimaryKeyRelatedField(
+        queryset=NotesGroup.objects.all()
+    )
+
+    created = serializers.HiddenField(default=now)
+    last_edited = serializers.HiddenField(default=now)
+    edited_by = serializers.HiddenField(default=CurrentUserPkDefault())
+    content = serializers.CharField(required=True, allow_blank=True)
 
     class Meta:
-        self_view = "notes:board-group-note-detail"
-        collection_name = ("notes__notes_group",)
+        self_view = "notes:groups-notes"
+        collection_name = "notes__groups"
 
 
-__all__ = ["BoardNotesSerializer", "GroupNotesSerializer"]
+__all__ = [
+    "BoardNotesSerializer", "GroupNotesSerializer"
+]
--- TimeManagerBackend/apps/notes/urls.py
+++ TimeManagerBackend/apps/notes/urls.py
@@ -1,20 +1,39 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
+from rest_framework_nested.routers import NestedSimpleRouter
 
 from .apps import CoreConfig as CurrentApp
 from .boards.views import NotesBoardViewSet, BoardMembersView
+from .groups.views import NotesGroupViewSet
+from .notes.views import BoardNotesView, GroupNotesView
 
 app_name = CurrentApp.name
 
 router = SimpleRouter()
-router.register(r"boards", NotesBoardViewSet, "boards")
+router.register(r"boards", NotesBoardViewSet, basename="boards")
+
+nested_router = NestedSimpleRouter(
+    router, r"boards", lookup="boards"  # translates to kwarg boards_pk
+)
+nested_router.register(r"groups", NotesGroupViewSet, basename="groups")
 
 urlpatterns = [
     path(
         "boards/<boards_pk>/members/",
         BoardMembersView.as_view(),
         name="boards-members"
+    ),
+    path(
+        "boards/<boards_pk>/notes/<pk>/",
+        BoardNotesView.as_view(),
+        name="boards-notes"
+    ),
+    path(
+        "boards/<boards_pk>/groups/<groups_pk>/notes/<pk>/",
+        GroupNotesView.as_view(),
+        name="groups-notes"
     )
 ]
 
 urlpatterns += router.urls
+urlpatterns += nested_router.urls
