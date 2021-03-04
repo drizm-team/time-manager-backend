74d944ad9ec49e31c7623934bb371217279d0e3d
ThaRising <kochbe.ber@gmail.com>
2021-03-04 20:19:18 +0100

    Parametrize notes tests for groups notes and boards notes

--- tests/integration/notes/test_notes.py
+++ tests/integration/notes/test_notes.py
@@ -1,97 +1,95 @@
 import uuid
+from operator import attrgetter
 
-from drizm_commons.testing.truthiness import all_keys_present
+from parameterized.parameterized import parameterized_class
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
 
-from TimeManagerBackend.apps.notes.models import Note
-from ...conftest import create_test_user, TEST_USER_PASSWORD
+from TimeManagerBackend.apps.notes.boards.models import NotesBoard
+from TimeManagerBackend.apps.notes.groups.models import NotesGroup
+from ...conftest import create_test_user
 
 
+@parameterized_class(
+    ("base_url", "url_args", "group"),
+    [
+        ("notes:boards-notes", (attrgetter("board.pk"),), None),
+        (
+                "notes:groups-notes",
+                (attrgetter("board.pk"), attrgetter("group.pk")),
+                {"title": "Some Group", "color": int("ffffff", 16)}
+        )
+    ]
+)
 class TestNotes(APITestCase):
     def setUp(self) -> None:
-        app_base = "notes:note-%s"
-        self.list = app_base % "list"
-        self.detail = app_base % "detail"
-
-        self.user_pw = TEST_USER_PASSWORD
         self.user = create_test_user()
-
-    def test010_create(self):
-        """
-        GIVEN I have a user account
-            AND I am logged in
-        WHEN I ask to create a new Note
-            AND I generate a UUID as a primary identifier for the Note
-        THEN I should be able to create a new note
-        """
-        pk = uuid.uuid4()
-        url = reverse(self.detail, args=(pk,))
-
-        # Should not work here because we are not logged in
-        res = self.client.post(url)
-        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
-
-        # Create a new note, while being logged in
-        # This should work
-        self.client.force_authenticate(user=self.user)
-        res = self.client.put(url, data={
-            "content": "Something else idk"
-        })
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
-        self.assertEqual(self.user, Note.objects.get(pk=pk).creator)
-        assert all_keys_present(res.json(), ("self", "content"))
-
-        # Create a note without specifying any body,
-        # thus not providing any content
-        # This should work
-        pk = uuid.uuid4()
-        url = reverse(self.detail, args=(pk,))
-        res = self.client.put(url)
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
-
-        # Create a new note while specifying empty content
-        # This should work
-        pk = uuid.uuid4()
-        url = reverse(self.detail, args=(pk,))
-        res = self.client.put(url, data={"content": ""})
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
-
-    def test020_overwrite(self):
-        """
-        GIVEN I have a user account
-            AND I am logged in
-        WHEN I ask to overwrite an existing note
-            AND that note was created by mself
-        THEN I should be able to overwrite an existing note
-        """
-        pk = uuid.uuid4()
-        url = reverse(self.detail, args=(pk,))
-
-        # Create a note as the test user
-        # This should work fine
-        self.client.force_authenticate(user=self.user)
-        res = self.client.put(url, data={
-            "content": "some Content :)"
-        })
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
-
-        # Overwrite the note from before
-        # This should work
-        res = self.client.put(url, data={
-            "content": "changed some stuff"
-        })
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
-
-        # Create a new test user and attempt to have the note
-        # overwritten by them, this should not work
-        test_user = create_test_user(
-            email="someMailmm@domain.com",
-            password="iuoiheriudeirutisertnh"
+        self.member = create_test_user(
+            "randomMail@okay.com", "iudbgiubrgiubeiru420"
         )
-        self.client.force_authenticate(user=test_user)
-        res = self.client.put(url, data={
-            "content": "I wanna overwrite this!"
+
+        self.board = NotesBoard.objects.create(
+            owner=self.user, title="Okay Son"
+        )
+        self.board.members.set([self.user])
+        self.board.save()
+
+        if self.group:
+            self.group = NotesGroup.objects.create(
+                parent=self.board, **self.group
+            )
+
+    def _get_url(self) -> str:
+        return reverse(
+            self.base_url,
+            [arg(self) for arg in self.url_args] + [str(uuid.uuid4())]
+        )
+
+    def test010_upsert(self):
+        """
+        GIVEN I have a user account
+            AND I am logged in
+        WHEN I ask to upsert a note
+            AND I am a member or the owner of the board that it belongs to
+            AND I generated a UUIDv4 as a primary identifier for the Note
+        THEN I should be able to create a note if it did not exist previously
+            OR update one that did exist previously
+        """
+        url = self._get_url()
+
+        self.client.force_authenticate(user=self.user)
+        res = self.client.put(url, {
+            "content": "literally no idea"
         })
-        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+        content = res.json()
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+        # Make sure updating works as expected
+        res = self.client.put(url, {
+            "content": "Changed the content"
+        })
+        new_content = res.json()
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertNotEqual(
+            new_content.get("last_edited"),
+            content.get("last_edited")
+        )
+
+    def test020_delete(self):
+        """
+        GIVEN I have a user account
+            AND I am logged in
+        WHEN I ask to delete a note
+            AND I am a member or the owner of the board that it belongs to
+        THEN I should be able to delete that note
+        """
+        url = self._get_url()
+
+        self.client.force_authenticate(user=self.user)
+        self.client.put(url, {
+            "content": "midrtniutnhoiudrntzoindrotunodrnzi setnieu5rz"
+        })
+
+        res = self.client.delete(url)
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
